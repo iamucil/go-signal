e7738fe01af1ef925801c8b595045e09778d34d4
iamucil <ecko.ucil@gmail.com>
2018-07-27 16:26:48 +0700

    update slack

--- main.go
+++ main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	slack "github.com/monochromegane/slack-incoming-webhooks"
 	// "github.com/iamucil/go-signal/signal"
 )
 
 func main() {
-	fmt.Println("vim-go")
-	// payload := {
-	// 	Text: "User ",
-	//	IconEmoji: ":calling:",
-	//}
-	//attachment := &{
-	//	Text: "Twilio",
-	//	Color: "#3AA3E3",
-	//	Title: "Twilio",
-	//}
-	// slack.Payload.AddAttachment(attachment)
+	payload := slack.Payload{
+		Text:      "User with phone number +phone-number has been registered into radiumchat",
+		IconEmoji: ":robot_face:",
+		Username:  "Radium-Bot",
+	}
+	attachment := &slack.Attachment{}
+	attachment.AuthorName = "Radium-messenger"
+	attachment.AuthorLink = "https://radium.id"
+	attachment.Title = "Twilio Payload"
+	attachment.Pretext = "Pretext Twilio Payload"
+	attachment.Text = "```Twilio Payload sender name ```"
+	attachment.Color = "#3AA3E3"
+	attachment.Fallback = "Twilio Payload."
+	attachment.FooterIcon = "https://platform.slack-edge.com/img/default_application_icon.png"
+	attachment.Footer = "Twilio-Messenger"
+
+	payload.AddAttachment(attachment)
 
 	slack.Client{
 		WebhookURL: "https://hooks.slack.com/services/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",
-	}.Post(&slack.Payload{
-		Text:      "User +phone has been registered into radiumchat.",
-		IconEmoji: ":calling:",
-	})
+	}.Post(&payload)
 }
