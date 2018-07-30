5010d357643e3c0f78d0e217285ff2be5ee55cd0
Eko Susilo <ecko.ucil@gmail.com>
2018-07-30 08:11:09 +0700

    serializer alice-bob

--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"fmt"
-	slack "github.com/monochromegane/slack-incoming-webhooks"
-	// "github.com/iamucil/go-signal/signal"
+
+	"github.com/RadicalApp/libsignal-protocol-go/util/keyhelper"
+	"github.com/iamucil/go-signal/signal"
 )
 
 func main() {
-	fmt.Println("vim-go")
-	// payload := {
-	// 	Text: "User ",
-	//	IconEmoji: ":calling:",
-	//}
-	//attachment := &{
-	//	Text: "Twilio",
-	//	Color: "#3AA3E3",
-	//	Title: "Twilio",
-	//}
-	// slack.Payload.AddAttachment(attachment)
-
-	slack.Client{
-		WebhookURL: "https://hooks.slack.com/services/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",
-	}.Post(&slack.Payload{
-		Text:      "User +phone has been registered into radiumchat.",
-		IconEmoji: ":calling:",
-	})
+	regID := keyhelper.GenerateRegistrationID()
+	fmt.Println(regID)
+	signal.Serializing()
 }
--- signal/signal.go
+++ signal/signal.go
@@ -1,1 +1,57 @@
 package signal
+
+import (
+	"fmt"
+
+	"github.com/RadicalApp/libsignal-protocol-go/keys/prekey"
+	"github.com/RadicalApp/libsignal-protocol-go/logger"
+	"github.com/RadicalApp/libsignal-protocol-go/session"
+	"github.com/RadicalApp/libsignal-protocol-go/state/record"
+	"github.com/kr/pretty"
+)
+
+func Serializing() {
+	serializer := newSerializer()
+
+	alice := newUser("Alice", 1, serializer)
+	bob := newUser("Bob", 1, serializer)
+
+	// create our users who will talk to each other
+	alice.buildSession(bob.address, serializer)
+	bob.buildSession(alice.address, serializer)
+
+	// fmt.Printf("%# v\n", bob)
+	retrivedPreKey := prekey.NewBundle(
+		bob.registrationID,
+		bob.deviceID,
+		bob.preKeys[0].ID(),
+		bob.signedPreKey.ID(),
+		bob.preKeys[0].KeyPair().PublicKey(),
+		bob.signedPreKey.KeyPair().PublicKey(),
+		bob.signedPreKey.Signature(),
+		bob.identityKeyPair.PublicKey(),
+	)
+	// fmt.Printf("Retrieved key: %# v\n", retrivedPreKey)
+	// process Bob's retrieved prekey to establish a session
+	alice.sessionBuilder.ProcessBundle(retrivedPreKey)
+
+	// create a session ciper to encrypt messages to bob
+	plainTextMessage := []byte("Hello")
+	sessionCipher := session.NewCipher(alice.sessionBuilder, bob.address)
+	fmt.Printf("Session Cipher: % #v\n", sessionCipher)
+	sessionCipher.Encrypt(plainTextMessage)
+
+	// Serialize our session so it can be stored
+	loadedSession := alice.sessionStore.LoadSession(bob.address)
+	serializedSession := loadedSession.Serialize()
+	logger.Debug(string(serializedSession))
+
+	// // try deserializing our session back into an object
+	deserializedSession, err := record.NewSessionFromBytes(serializedSession, serializer.Session, serializer.State)
+	if err != nil {
+		logger.Error("Failed to deserilize session")
+	}
+
+	fmt.Printf("Original Session Record: %# v\n", pretty.Formatter(loadedSession))
+	fmt.Printf("Deserialized Session Record: %# v\n", pretty.Formatter(deserializedSession))
+}
