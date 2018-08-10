7763fe4a7805d9376b82a5e54e557c3f30b1a34a
iamucil <ecko.ucil@gmail.com>
2018-08-10 17:06:06 +0700

    remove slack-incoming-webhook from main package

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/RadicalApp/libsignal-protocol-go/util/keyhelper"
 	"github.com/iamucil/go-signal/signal"
-	slack "github.com/monochromegane/slack-incoming-webhooks"
+	// slack "github.com/monochromegane/slack-incoming-webhooks"
 )
 
 func main() {
@@ -14,25 +14,25 @@ func main() {
 	signal.Serializing()
 	signal.Fingerprint()
 
-	payload := slack.Payload{
-		Text:      "User with phone number +phone-number has been registered into radiumchat",
-		IconEmoji: ":robot_face:",
-		Username:  "Radium-Bot",
-	}
-	attachment := &slack.Attachment{}
-	attachment.AuthorName = "Radium-messenger"
-	attachment.AuthorLink = "https://radium.id"
-	attachment.Title = "Twilio Payload"
-	attachment.Pretext = "Pretext Twilio Payload"
-	attachment.Text = "```Twilio Payload sender name ```"
-	attachment.Color = "#3AA3E3"
-	attachment.Fallback = "Twilio Payload."
-	attachment.FooterIcon = "https://platform.slack-edge.com/img/default_application_icon.png"
-	attachment.Footer = "Twilio-Messenger"
+	// payload := slack.Payload{
+	// 	Text:      "User with phone number +phone-number has been registered into radiumchat",
+	// 	IconEmoji: ":robot_face:",
+	// 	Username:  "Radium-Bot",
+	// }
+	// attachment := &slack.Attachment{}
+	// attachment.AuthorName = "Radium-messenger"
+	// attachment.AuthorLink = "https://radium.id"
+	// attachment.Title = "Twilio Payload"
+	// attachment.Pretext = "Pretext Twilio Payload"
+	// attachment.Text = "```Twilio Payload sender name ```"
+	// attachment.Color = "#3AA3E3"
+	// attachment.Fallback = "Twilio Payload."
+	// attachment.FooterIcon = "https://platform.slack-edge.com/img/default_application_icon.png"
+	// attachment.Footer = "Twilio-Messenger"
 
-	payload.AddAttachment(attachment)
+	// payload.AddAttachment(attachment)
 
-	slack.Client{
-		WebhookURL: "https://hooks.slack.com/services/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",
-	}.Post(&payload)
+	// slack.Client{
+	// 	WebhookURL: "https://hooks.slack.com/services/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",
+	// }.Post(&payload)
 }
